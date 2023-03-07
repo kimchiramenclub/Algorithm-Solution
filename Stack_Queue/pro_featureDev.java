a0fc5b6d98c5208623220c98d56948b638a4eebb
joon <june.w.lim@gmail.com>
2023-03-08 01:13:49 +0900

    프로그래머스 스택, 큐 1,2번  커밋

---
+++ Stack_Queue/pro_featureDev.java
@@ -0,0 +1,37 @@
+package Stack_Queue;
+// https://velog.io/@kimbad1992/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B8%B0%EB%8A%A5%EA%B0%9C%EB%B0%9C
+
+import java.util.Scanner;
+
+public class pro_featureDev {
+    public static void main(String[] args) {
+        // 테스트 코드
+        int[] progresses = new int[5];
+        int[] speeds = new int[5];
+
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            progresses[i] = in.nextInt();
+        }
+        for (int i = 0; i < 5; i++) {
+            speeds[i] = in.nextInt();
+        }
+
+        int[] releaseDateCount = new int[100];
+        int day = -1;
+
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + speeds[i] * day < 100){
+                day++;
+            }
+            releaseDateCount[day]++;
+        }
+
+
+        // 바꾸는 과정이 메모리를 많이 잡아먹는다함. for문으로 고치는 게 메모리상 효율적
+        // int[] answer = Arrays.stream(releaseDateCount).filter(i -> i!=0).toArray();
+
+
+
+    }
+}
---
+++ Stack_Queue/pro_noSameNumber.java
@@ -0,0 +1,62 @@
+package Stack_Queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class pro_noSameNumber {
+    public int[] solution(int []arr) {
+        int[] answer;
+
+        // 큐 인스턴스. 지속적인 수정/삭제 가 없으므로 ArrayDeque가 메모리 상으로 더 유리.
+        Deque<Integer> deque = new ArrayDeque<>();
+
+
+
+        // arr 배열을 큐에 넣으면서, 직전 같은 값이 있는 지( 체크 후, 없으면 pass
+        for(int i=0;i<arr.length;i++) {
+            if(i > 0 && arr[i] == deque.peekLast()) {
+                continue;
+            }
+            deque.addLast(arr[i]);
+        }
+        // 큐를 answer 배열에 담음.
+        answer = new int[deque.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = deque.poll();
+        }
+
+
+        return answer;
+    }
+
+
+    // 테스트케이스
+    /*public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[7];
+        for(int i=0;i<arr.length;i++) {
+           arr[i] = sc.nextInt();
+        }
+
+
+        // 큐 인스턴스
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // arr 배열을 큐에 넣으면서, 직전 같은 값이 있는 지( 체크 후, 없으면 pass
+        for(int i=0;i<arr.length;i++) {
+            if(i > 0 && arr[i] == deque.peekLast()) {
+                continue;
+            }
+            deque.addLast(arr[i]);
+        }
+        // 큐를 answer 배열에 담음.
+
+        int[] answer = new int[deque.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = deque.poll();
+            System.out.print(answer[i]+" ");
+        }
+
+    }*/
+}
