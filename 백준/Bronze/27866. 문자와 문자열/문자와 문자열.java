e4c6398ef294342e55a095428ba5ceab1a4bec61
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-09-01 15:40:58 +0900

    [Bronze V] Title: 문자와 문자열, Time: 132 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        System.out.println(S.charAt(Integer.parseInt(br.readLine()) - 1));
+    }
+}
