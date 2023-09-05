3dc2853df464b6437cb0e015f6275032c67c1e30
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-09-05 20:50:09 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11216 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |");
+
+        System.out.println(sb);
+    }
+}
