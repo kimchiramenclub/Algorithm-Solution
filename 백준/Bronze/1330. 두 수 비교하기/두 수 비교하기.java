1ea1f11e35568cce6a762bc375acdf7d13354a56
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-09-12 19:49:49 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 76 ms, Memory: 11428 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,22 +1,24 @@
 import java.io.IOException;
 
-public class Main {
+class Main {
 
-    public static void main(String[] args) throws Exception {
 
+    public static void main(String[] args) throws IOException {
         int A = readInt();
         int B = readInt();
-        int result = Integer.compare(A, B);
-        if(result < 0) System.out.println("<");
-        else if(result == 0) System.out.println("==");
-        else System.out.println(">");
+
+        if(A < B) System.out.println("<");
+        else if(A > B) System.out.println(">");
+        else System.out.println("==");
+
+
     }
 
     static int readInt() throws IOException {
-        int c, sign = 1, n = 0;
+        int c, n = 0, sign = 1;
         if((c = System.in.read()) == '-') sign = -1;
-        else n = c & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 2) + (c & 15);
+        else n = c;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n * sign;
     }
 }
