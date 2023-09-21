c8080b63a262cac11c5df010d19c93c901f744cc
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-09-21 21:52:04 +0900

    [Silver V] Title: 소트인사이드, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -5,22 +5,23 @@ class Main {
     /*  정렬
      * */
 
-    static ArrayList<Integer> array = new ArrayList<>(10);
+    static int[] array = new int[10];
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
 
-        readDigits();
-        array.sort(Collections.reverseOrder()); // 배열 정렬
+        int lastIdx = readDigits();
+        Arrays.sort(array, 0, lastIdx + 1);
 
         // array의 각 index에 저장된 숫자의 'ascii' 숫자대로, 해당 digit을 print
-        for(int num : array) System.out.write(num);
+        for(int i = lastIdx; i>=0; i--) System.out.write(array[i]);
         System.out.flush();
     }
 
     // 입력에서 한 자리 숫자를 읽고, 해당 ascii number로 저장하는 메서드
-    static void readDigits() throws IOException {
-        int c;
-        while((c = System.in.read()) >= 32) array.add(c);
+    static int readDigits() throws IOException {
+        int c, i = -1;
+        while((c = System.in.read()) >= 32) array[++i] = c;
+        return i;
     }
 }
