fb72cf8b0169da716724c344fdf7e9e54d5621aa
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-10-10 18:57:13 +0900

    [Gold IV] Title: 최단경로, Time: 404 ms, Memory: 30484 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,6 +4,7 @@ import java.util.*;
 class Main {
     /*      다익스트라 알고리즘 - 최단경로
 
+            - 2번째 풀이. distance를 따로 뺌
             - 두 정점 사이에 복수의 간선 있을 수도 있음.
             - '방향'그래프 이기 때문에, 입력에서 받는 간선 정보가 단방향임.
 
@@ -13,33 +14,38 @@ class Main {
     * */
 
     static List<Edge>[] graph;  // 방향 그래프
-    static Vertex[] vertices;   // 정점 배열
+    static int[] dist;   // 정점 배열
 
     static void solution(int V, int E, int K) throws IOException {
         StringBuilder sb = new StringBuilder();
 
-        PriorityQueue<Vertex> pq = new PriorityQueue<>();
-        vertices[K].distance = 0;   // 시작정점까지의 거리 0으로
-        pq.offer(vertices[K]); // 시작정점 입력
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        dist[K] = 0;    // 시작정점 거리 0 입력
+        pq.offer(new Edge(K, 0)); // 시작정점 입력
 
         while(!pq.isEmpty()){
-            Vertex current = pq.poll(); // 현재 정점
+            Edge current = pq.poll();
+            int node = current.node;
 
-            for(Edge e : graph[current.id]){
-                int newDist = current.distance + e.weight;  // 이전 정점의 거리 + 가중치
+            // 방문체크 + 지금까지 구한 최단거리가 현재 루트보다 짧다면, pass
+            if(dist[node] < current.weight) continue;
+
+            for(Edge next : graph[current.node]){
+                int nextWeight = current.weight + next.weight;  // 현재 정점까지의 가중치(거리) + 다음 정점까지의 가중치
 
                 // A -> B -> C > A -> C 라면, 굳이 더 돌아가는 루트는 최소 거리 계산에 필요없으므로 배제.
-                if(newDist < e.target.distance){    // 최소 거리라면 업데이트
-                    e.target.distance = newDist;
-                    pq.offer(e.target);     // 다음 정점으로 이동
+                if(dist[next.node] > nextWeight){    // 최소 거리라면 업데이트
+                    dist[next.node] = nextWeight;
+                    // 현재까지 이동한 거리(가중치)를 수정하며 다음 정점으로 이동
+                    pq.offer(new Edge(next.node, nextWeight));
                 }
             }
         }
 
         // 시작 정점으로부터 해당 정점까지의 최단 경로 출력
         for(int i=1;i<=V;i++){
-            if(vertices[i].distance == Integer.MAX_VALUE) sb.append("INF");
-            else sb.append(vertices[i].distance);
+            if(dist[i] == Integer.MAX_VALUE) sb.append("INF");
+            else sb.append(dist[i]);
             sb.append("\n");
         }
         System.out.println(sb);
@@ -53,16 +59,16 @@ class Main {
         // graph 선언
         graph = new ArrayList[V+1];
         for(int i=0;i<=V;i++) graph[i] = new ArrayList<>();
-        // Vertex 선언
-        vertices = new Vertex[V+1];
-        for(int i=1;i<=V;i++) vertices[i] = new Vertex(i, Integer.MAX_VALUE);
+
+        dist = new int[V+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
         for(int i=0;i<E;i++){
             int u = readInt();  // 시작정점
             int v = readInt();  // 도착정점
             int w = readInt();  // 간선 간의 가중치
             // 간선 정보 입력. '방향'그래프라서, 단방향 정보만 입력
-            graph[u].add(new Edge(vertices[v], w));
+            graph[u].add(new Edge(v, w));
         }
 
         solution(V, E, K);
@@ -75,34 +81,19 @@ class Main {
         return n;
     }
 
-    // 초반에는 간선이 이어진 정점의 id를 제공하는 데 쓰이고, 나중에는 해당 정점까지의 distance를 의미함.
-    // distance 정보는 '시작 정점 -> 해당 정점까지의 거리' 를 의미하므로, 시작정점이 '1'이라면 Vertex 1의 거리는 무조건 0
-    // Vertex는 각 정점 마다 unique 하고, 해당 정점으로의 간선이 여러 개 존재하더라도 같은 vertex를 가져다 쓰는거임.
-    static class Vertex implements Comparable<Vertex> {
-        public final int id;  // 정점 번호
-        public int distance;  // 해당 정점까지의 최단 경로
+    static class Edge implements Comparable<Edge>{
+        public final int node;      // 정점 번호
+        public final int weight;    // 가중치
 
-        public Vertex(int id, int distance) {   // 해당 정점까지의 거리 업데이트
-            this.id = id;
-            this.distance = distance;
-        }
-
-        @Override
-        public int compareTo(Vertex other) {    // PriorityQueue의 정렬 기준
-            return Integer.compare(this.distance, other.distance);
-        }
-    }
-
-    // JAVA 14 버전 이후로는 record로 표현가능.
-    // record Edge(Vertex target, int weight) {}
-    // 프로그램에서 불변 데이터를 모델링하는 데 사용. 단순한 데이터 전달자 역할을 하는 클래스를 선언하는 보다 간결하고 명확한 방법
-    static class Edge{
-        public final Vertex target;
-        public final int weight;
-
-        public Edge(Vertex target, int weight){
-            this.target = target;
+        public Edge(int node, int weight){
+            this.node = node;
             this.weight = weight;
+        }
+
+        // PriorityQueue의 정렬기준
+        @Override
+        public int compareTo(Edge edge) {
+            return this.weight - edge.weight;
         }
     }
 }
