8d9f0fa3716f4eac00aa4fb9718f57bb64b78cb6
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-10-11 01:40:09 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,38 +1,35 @@
 import java.io.*;
-import java.util.*;
 
 class Main {
     /*
      * */
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         boolean[] alphabet = new boolean[27];       // 그룹단어 판별용 array. 'a'~'z' -> 1~26의 idx에 해당됨
         int count = 0;  // 그룹단어 갯수
 
-        for (int i = 0; i < N; i++) {
-            String word = br.readLine();
-            int lastIdx = 0;    // 이전 char의 idx 기억용
-            boolean notGroup = false;   // true면 group단어가 아님
+        while (N-- > 0) if (check()) count++;
+        System.out.println(count);  // 그룹단어 갯수 출력
+    }
 
-            // 그룹단어 판정
-            for (int j = 0; j < word.length(); j++) {
-                int idx = word.charAt(j) & 31;  // 'a'~'z' & 15의 연산 결과 : 1 ~ 26
-                if(!alphabet[idx]) alphabet[idx] = true;    // 문자가 처음 나타났다면
-                else if(alphabet[idx] && idx == lastIdx) continue;  // 연속해서 나타난 문자라면
-                else{   // 문자가 연속되지 않게 나타났다면, 그룹단어가 아니므로 break
-                    notGroup = true;
-                    break;
+    // 그룹단어 판정
+    static boolean check() throws IOException {
+        String word = br.readLine();
+        boolean[] chars = new boolean[27];  // 그룹단어 판별용 array. 'a'~'z' -> 1~26의 idx에 해당됨
+        int prev = 0; // 이전 char check용
+
+        for (int i = 0; i < word.length(); i++) {
+            if (prev != word.charAt(i)) { // 연속되지 않은 문자
+                if (chars[word.charAt(i) & 31]) return false;    // 이전에 발견한 문자라면, 그룹단어가 아니므로 break
+                else {   // 문자가 처음 나타났다면
+                    chars[word.charAt(i) & 31] = true;
+                    prev = word.charAt(i);
                 }
-                lastIdx = idx;
             }
-
-            if(!notGroup) count++;
-            Arrays.fill(alphabet, false);   
         }
-        // 그룹단어 갯수 출력
-        System.out.println(count);
+        return true;
     }
 }
