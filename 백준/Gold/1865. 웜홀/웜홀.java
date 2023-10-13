42e1e3d61205695d6f324f1e01ba9b81634e50c3
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-10-14 01:15:42 +0900

    [Gold III] Title: 웜홀, Time: 192 ms, Memory: 29244 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,77 +1,110 @@
-import java.util.stream.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
-import static java.util.Arrays.*;
-import static java.util.stream.Collectors.*;
+class Main {
+    /*  벨만-포드 알고리즘 : 단방향 음수 가중치
+        - 음의 사이클이 있기만 하면 YES, 없으면 NO
 
-import java.util.*;
-import java.io.*;
+        - 시작 노드를 1~N으로 잡으면, 어떤 시작 노드에서 음의 사이클이 나타날 지 몰라서 벨만-포드 알고리즘 자체도 N번 돌려야함.
+        - 이 문제에서는 음의 사이클 여부만 찾고 있음. 따라서, 실제 거리가 어떻게 갱신되는지는 중요치 않음.
+        - 가상 시작 노드 0을 잡고, 모든 노드로 0의 가중치로 도로가 연결되게 하면, 맨 처음에 모든 dist 값이 0으로 갱신됨.
+        - 그럼에도 벨만-포드 알고리즘을 돌릴 시 갱신되는 값이 있다면, 음의 사이클이 있는 것.
+     *  */
 
+    static Node[] nodes;
 
-public class Main {
+    static boolean bellmanFord(int N) {
+        int[] dist = new int[N + 1];
+        boolean update = false;
 
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int n;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        while (tc-->0){
-             int[] input = stream(br.readLine().split(" "))
-                      .mapToInt(Integer::parseInt).toArray();
-             n = input[0];
-             int m = input[1], w= input[2];
-
-            graph = new ArrayList<>();
-            IntStream.rangeClosed(0,n).forEach(e->graph.add(new ArrayList<>()));
-
-            for(int i=0;i<m+w;i++){
-                 input = stream(br.readLine().split(" "))
-                          .mapToInt(Integer::parseInt).toArray();
-                if(i<m){
-                    graph.get(input[0]).add(new Node(input[1],input[2]));
-                    graph.get(input[1]).add(new Node(input[0],input[2]));
-                }else
-                    graph.get(input[0]).add(new Node(input[1],-1*input[2]));
-            }//end input
-
-            if(bellmanford())
-                System.out.println("YES");
-            else
-                System.out.println("NO");
-
-        }
-
-    }
-
-    private static boolean bellmanford(){
-
-        int[] distance = new int[n+1]; // distance 배열 0으로 초기화
-
-        boolean update =false;
-
-        for(int i=1; i<=n; i++){ // 수행횟수 : n-1(다익스트라) +1(최종확인) = n번
-            update =false;
-            for(int cur=1; cur<=n; cur++){  
-
-                for (Node next : graph.get(cur)) {
-                    if(distance[next.node] > distance[cur]+next.weight){
-                        distance[next.node] = distance[cur]+next.weight;
-                        update = true; 
+        // N-1번 간선 순회 반복 작업 후, 마지막 음의 사이클 확인 작업
+        for (int i = 1; i <= N; i++) {
+            update = false;
+            for (int j = 1; j <= N; j++) {
+                for (Edge edge : nodes[j].edges) {
+                    if (dist[edge.start] + edge.weight < dist[edge.end]) {
+                        dist[edge.end] = dist[edge.start] + edge.weight;
+                        if (!update) update = true;
                     }
                 }
             }
-            if(!update) // 음의 사이클이 없는 경우
-                break;
+            if (!update) break;
         }
-        return update;
+        
+        return update; // 음수 사이클이 없음
     }
 
-    static class Node{
-        int node,weight;
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
 
-        public Node(int node, int weight) {
-            this.node = node;
+        int TC = readInt();
+
+        for (int i = 0; i < TC; i++) {
+            int N = readInt();  // 노드 갯수
+            int M = readInt();  // 간선 갯수
+            int W = readInt();  // 웜홀 갯수
+
+            nodes = new Node[N + 1];
+            for (int j = 1; j <= N; j++) nodes[j] = new Node();
+
+            // 도로 입력
+            for (int j = 0; j < M; j++) {
+                int S = readInt();
+                int E = readInt();
+                int T = readInt();
+                nodes[S].addEdge(S, E, T);
+                nodes[E].addEdge(E, S, T); // 양방향 도로
+            }
+            // 웜홀 입력
+            for (int j = 0; j < W; j++) {
+                int S = readInt();
+                int E = readInt();
+                int T = readInt();
+                nodes[S].addEdge(S, E, -T); // 웜홀은 단방향 도로
+            }
+
+            // 음의 사이클이
+            sb.append(bellmanFord(N) ? "YES" : "NO").append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    static class Node {
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        void addEdge(int start, int end, int weight) {
+            edges.add(new Edge(start, end, weight));
+        }
+    }
+
+    static class Edge {
+        int start, end, weight;
+
+        Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
             this.weight = weight;
         }
+    }
+
+    static int idx, size, SIZE = 1 << 13;
+    static byte[] buf = new byte[SIZE];
+
+    static int readInt() throws IOException {
+        int n = 0;
+        byte c;
+        while ((c = read()) <= 32) ;
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (47 < (c = read()) && c < 58);
+        return n;
+    }
+
+    static byte read() throws IOException {
+        if (size == idx) {
+            size = System.in.read(buf, idx = 0, SIZE);
+            if (size < 0) buf[0] = -1;
+        }
+        return buf[idx++];
     }
 }
