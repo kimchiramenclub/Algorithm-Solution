6cd550de3cddfc480b55afdf0b630309a122c03f
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-10-27 23:48:03 +0900

    [Gold V] Title: 치킨 배달, Time: 112 ms, Memory: 12596 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -84,23 +84,9 @@ class Main {
     }
 
     // 입력 관련
-    static int idx, size, SIZE = 1 << 13;
-    static byte[] buf = new byte[SIZE];
-
     static int readInt() throws IOException {
-        int n = 0;
-        byte c;
-        while ((c = read()) <= 32) ;
-        do n = (n << 3) + (n << 1) + (c & 15);
-        while (47 < (c = read()) && c < 58);
+        int c, n = 0;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
-    }
-
-    static byte read() throws IOException {
-        if (size == idx) {
-            size = System.in.read(buf, idx = 0, SIZE);
-            if (size < 0) buf[0] = -1;
-        }
-        return buf[idx++];
     }
 }
