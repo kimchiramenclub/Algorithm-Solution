d36844cfdb826b2dfeafa1294cd7496504520ef3
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-12-03 22:16:16 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 80 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int c, n=0;
+        while((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        System.out.println(n-543);
+    }
+}
