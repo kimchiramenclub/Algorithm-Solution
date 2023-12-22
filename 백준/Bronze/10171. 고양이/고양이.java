265091b57c80d4ac416d830398da705e476a83ed
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-12-22 23:49:15 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+        System.out.print(sb);
+    }
+}
