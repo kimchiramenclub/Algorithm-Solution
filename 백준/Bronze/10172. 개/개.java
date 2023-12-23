fc5bc3ad46051dbfc905061beb7300302e406483
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-12-24 02:09:17 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|");
+        System.out.print(sb);
+    }
+}
