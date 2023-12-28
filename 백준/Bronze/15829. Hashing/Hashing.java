01e885cdabcf58665bc36e293de4f6ff5bcec1d5
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2023-12-29 01:03:29 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11468 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,17 +2,18 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int c, L = 0, ans = 0;
+        int c, L = 0;
+        long ans = 0;
         while ((c = System.in.read()) > 32) L = (L << 3) + (L << 1) + (c & 15);
 
         String S = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        for (int i = 0; i < L; i++) ans += getHash(S.charAt(i), i);
+        for (int i = 0; i < L; i++) ans = (ans + getHash(S.charAt(i), i)) % 1234567891;
         System.out.println(ans);
     }
 
-    static int getHash(char C, int hashIdx){
-        int num = (C - 96);
-        while(hashIdx-- > 0) num = (num * 31) % 1234567891;
+    static long getHash(char C, int hashIdx) {
+        long num = (C - 96);
+        while (hashIdx-- > 0) num = (num * 31) % 1234567891;
         return num;
     }
 }
