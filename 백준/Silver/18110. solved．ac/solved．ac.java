08fccd37a10af4ee2e6000711185e66380dd75b1
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2024-01-03 23:36:31 +0900

    [Silver IV] Title: solved.ac, Time: 108 ms, Memory: 12220 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -4,25 +4,37 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) throws IOException {
         int n = readInt();
-        int excludeNum = (n * 3) % 20 < 10 ? (n * 3) / 20 : (n * 3) / 20 + 1;
+        int excludeNum = (int) Math.round((double) n * 0.15);
         int lowDiff = 1, highDiff = 30;
         int[] levels = new int[31];
 
         for (int i = 0; i < n; i++) levels[readInt()]++;
 
-        for (int i = excludeNum; i > 0; i--) {
+        for (int i = excludeNum; i > 0;) {
             while (levels[lowDiff] == 0) lowDiff++;
-            while (levels[highDiff] == 0) highDiff--;
-            levels[lowDiff]--;
-            levels[highDiff]--;
+            if(levels[lowDiff] <= i){
+                i -= levels[lowDiff];
+                levels[lowDiff] = 0;
+            } else{
+                levels[lowDiff] -= i;
+                i = 0;
+            }
         }
 
-        int level = (int) Math.round(
-                (double) IntStream.range(1, 31)
-                        .map(i -> i * levels[i])
-                        .sum() / (n - excludeNum * 2)
-        );
-        System.out.println(level);
+        for (int i = excludeNum; i > 0;) {
+            while (levels[highDiff] == 0) highDiff--;
+            if(levels[highDiff] <= i){
+                i -= levels[highDiff];
+                levels[highDiff] = 0;
+            } else{
+                levels[highDiff] -= i;
+                i = 0;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= 30; i++) sum += (levels[i] * i);
+        System.out.println((int)Math.round((double) sum / (n - excludeNum * 2)));
     }
 
     // 입력 관련
