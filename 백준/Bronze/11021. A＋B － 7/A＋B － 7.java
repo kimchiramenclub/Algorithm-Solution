fe4668ed9a30beb1ff511d7c9dcc6ea2c7b00a5e
Joon <109577311+kimchiramenclub@users.noreply.github.com>
2024-01-07 23:08:50 +0900

    [Bronze V] Title: A+B - 7, Time: 80 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int T = readInt();
+
+        for (int i = 1; i <= T; i++) {
+            int A = readInt(), B = readInt();
+            sb.append("Case #"+i+": "+(A+B)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    // 입력 관련
+    static int idx, size, SIZE = 1 << 13;
+    static byte[] buf = new byte[SIZE];
+
+    static int readInt() throws IOException {
+        int n = 0;
+        byte c;
+        while ((c = read()) <= 32) ;
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (47 < (c = read()) && c < 58);
+        return n;
+    }
+
+    static byte read() throws IOException {
+        if (size == idx) {
+            size = System.in.read(buf, idx = 0, SIZE);
+            if (size < 0) buf[0] = -1;
+        }
+        return buf[idx++];
+    }
+}
